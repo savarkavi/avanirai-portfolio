3d705be486961691e5d63817d09e25ccd7894e87
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-09-30 15:26:22 +0530

    add dymaic route for category page

---
+++ client/src/app/(frontend)/archive/[category]/page.tsx
@@ -0,0 +1,25 @@
+import WorkList from "@/app/components/WorkList";
+import { sanityFetch } from "@/sanity/lib/live";
+import { FETCH_CATEGORY_PROJECTS } from "@/sanity/lib/queries";
+import React from "react";
+
+const Page = async ({ params }: { params: Promise<{ category: string }> }) => {
+  const { category } = await params;
+
+  const { data } = await sanityFetch({
+    query: FETCH_CATEGORY_PROJECTS,
+    params: {
+      category: category,
+    },
+  });
+
+  console.log(data);
+
+  return (
+    <div className="flex h-[calc(100vh-64px)]">
+      <WorkList data={data} />
+    </div>
+  );
+};
+
+export default Page;
