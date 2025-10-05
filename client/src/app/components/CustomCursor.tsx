6c6a520c513e5850323460149998478529819059
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-10-05 15:30:15 +0530

    feat: add CustomCursor component and removed default cursor

---
+++ client/src/app/components/CustomCursor.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export default function CustomCursor() {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const updatePosition = (e: MouseEvent) => {
+      setPosition({ x: e.clientX, y: e.clientY });
+    };
+
+    window.addEventListener("mousemove", updatePosition);
+    return () => window.removeEventListener("mousemove", updatePosition);
+  }, []);
+
+  return (
+    <div
+      className="pointer-events-none fixed z-99 hidden h-3 w-3 rounded-full bg-white mix-blend-difference 2xl:block"
+      style={{
+        left: `${position.x}px`,
+        top: `${position.y}px`,
+        transform: "translate(-50%, -50%)",
+      }}
+    />
+  );
+}
--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import HeaderWrapper from "./components/HeaderWrapper";
 import { ReactLenis } from "lenis/react";
+import CustomCursor from "./components/CustomCursor";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <CustomCursor />
         <ReactLenis root>{children}</ReactLenis>
       </body>
     </html>
