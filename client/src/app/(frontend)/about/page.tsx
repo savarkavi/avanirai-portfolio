80e6e816ad580f4fc4fac5a6768ea9318676e49b
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-10-21 06:29:59 +0530

    add temporary about page

---
+++ client/src/app/(frontend)/about/page.tsx
@@ -0,0 +1,20 @@
+import { oldNewsPaper } from "@/fonts/fonts";
+import Image from "next/image";
+import React from "react";
+
+const Page = () => {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center">
+      <div className="relative h-screen w-screen">
+        <Image src="/about.gif" alt="image" fill className="object-cover" />
+      </div>
+      <p
+        className={`${oldNewsPaper.className} absolute top-1/2 left-1/2 -translate-1/2 text-3xl text-white`}
+      >
+        Comming Soon
+      </p>
+    </div>
+  );
+};
+
+export default Page;
